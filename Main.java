29a16699b77079ce12da8dafb52829cb699e2209
Juggal347 <99968861+Juggal347@users.noreply.github.com>
2022-05-12 13:25:48 +0530

    Java program to compare two strings lexicographically, ignoring case differences.

--- Main.java
+++ Main.java
@@ -7,18 +7,18 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first string:");
+        System.out.println("Enter a string:");
         String str1 = sc.nextLine();
 
         System.out.println("Enter second string:");
         String str2 = sc.nextLine();
 
-        int result = str1.compareTo(str2);
+        int result = str1.compareToIgnoreCase(str2);
 
         if (result<0)
             System.out.println(str1+" is less than "+str2);
 
-        else if (result==0)
+        else if (result == 0)
             System.out.println(str1+" is equal to "+str2);
 
         else
